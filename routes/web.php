05d3ea9eec5b7aea3261ff669d0376e6e8a374a3
Aflah Fadlurrahman <aflah24ti@mahasiswa.pcr.ac.id>
2025-09-29 15:24:22 +0700

    laporan prak 4

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -28,3 +29,5 @@ Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
 Route::get('/about', function () {
     return view('halaman-about');
 });
+
+Route::get('/home', [HomeController::class, 'index']);
