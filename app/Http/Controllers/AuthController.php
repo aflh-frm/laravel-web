67c7337e6b234895939ba421daa672edf6609dfe
aflah fadlurrahman <aflah24ti@mahasiswa.pcr.ac.id>
2025-10-06 15:09:23 +0700

    Pertemuan 5

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,18 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
+class AuthController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-            /* Cara 1 */
-        $data['username']       = 'Aflah Fadlurrahman';
-        $data['last_login']     = date('Y-m-d H:i:s');
-        $data['list_pendidikan'] = ['SD', 'SMP', 'SMA', 'S1', 'S2', 'S3'];
-        return view('home', $data);
+        //
     }
 
     /**
