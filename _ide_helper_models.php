be7180ff8f3cd8852fd50945fb61be9e34a7c3b8
aflh-frm <aflah24ti@mahasiswa.pcr.ac.id>
2025-12-04 00:16:57 +0700

    Pertemuan 13

---
+++ _ide_helper_models.php
@@ -0,0 +1,91 @@
+<?php
+
+// @formatter:off
+// phpcs:ignoreFile
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Multipleuploads newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Multipleuploads newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Multipleuploads query()
+ */
+	class Multipleuploads extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * @property int $pelanggan_id
+ * @property string $first_name
+ * @property string $last_name
+ * @property string|null $birthday
+ * @property string|null $gender
+ * @property string $email
+ * @property string|null $phone
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Pelanggan filter($request, array $filterableColumns)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Pelanggan newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Pelanggan newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Pelanggan query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Pelanggan search($request, array $columns)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Pelanggan whereBirthday($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Pelanggan whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Pelanggan whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Pelanggan whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Pelanggan whereGender($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Pelanggan whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Pelanggan wherePelangganId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Pelanggan wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Pelanggan whereUpdatedAt($value)
+ */
+	class Pelanggan extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $avatar
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $profile_picture
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Spatie\Permission\Models\Permission> $permissions
+ * @property-read int|null $permissions_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Spatie\Permission\Models\Role> $roles
+ * @property-read int|null $roles_count
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User permission($permissions, $without = false)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User role($roles, $guard = null, $without = false)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereAvatar($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereProfilePicture($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User withoutPermission($permissions)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User withoutRole($roles, $guard = null)
+ */
+	class User extends \Eloquent {}
+}
+
