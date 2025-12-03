64ccfc7498ec391b9515075d200bd06241d61885
aflh-frm <aflah24ti@mahasiswa.pcr.ac.id>
2025-12-04 00:05:43 +0700

    Pertemuan 13

--- database/migrations/2025_12_01_080037_add_avatar_to_users_table.php
+++ database/migrations/2025_12_01_080037_add_avatar_to_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('avatar')->nullable()->after('password'); // Kolom baru
+            $table->string('avatar')->nullable()->after('password');
         });
     }
 
